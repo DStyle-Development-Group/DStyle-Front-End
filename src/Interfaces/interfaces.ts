adb283b509a93440fecb4dcf9b790b6995ebdb2f
Toastalot <115156735+toastalot@users.noreply.github.com>
2023-05-22 13:50:00 +0100

    added comment to explain use of global interface Window

--- src/Interfaces/interfaces.ts
+++ src/Interfaces/interfaces.ts
@@ -1,5 +1,7 @@
 import type { providers } from 'ethers'
 
+// required because metamasks injects an api for ethereum access
+// using the window.ethereum provider object
 declare global {
 	interface Window {
 		ethereum: EthersProvider
